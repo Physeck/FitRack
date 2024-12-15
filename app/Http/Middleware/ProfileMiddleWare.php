c614ecb04d4956313cc4dda09d5d5ccf23ecd713
shichikuu <shichikuu@outlook.com>
2024-12-15 20:07:23 +0700

    middleware update

--- app/Http/Middleware/ProfileMiddleWare.php
+++ app/Http/Middleware/ProfileMiddleWare.php
@@ -8,27 +8,19 @@ use Symfony\Component\HttpFoundation\Response;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
-class PlanningMiddleWare
+class ProfileMiddleWare
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
-        // Check if the user is not logged in
         $user = User::find(Auth::id());
-
-        if ($user && ($user->height == 0 || $user->weight == 0 || !$user->gender)) {
-            return redirect()->route('prompt_user_data', ['redirect' => $request->path()]);
-        }
-
         if (!$user) {
             return redirect()->route('signup')->with('error', 'Please sign up or sign in to continue.');
         }
-
-        // If user is authenticated, allow to proceed
         return $next($request);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -26,15 +26,15 @@ Route::get('/aboutus', function () {
 
 Route::get('/profile', function () {
     return view('profile');
-})->middleware(\App\Http\Middleware\PlanningMiddleWare::class);;
+})->middleware(\App\Http\Middleware\ProfileMiddleWare::class);;
 Route::post('/profile/updateProfilePage', [UserController::class, 'updateProfilePage'])->name('profile.updateProfilePage');
 Route::post('/profile/updateProfile', [UserController::class, 'editProfile'])->name('profile.updateProfile');
 Route::post('/profile/verifyPassword', [UserController::class, 'verifyPassword'])->name('profile.verifyPassword');
 Route::post('/profile/updatePassword', [UserController::class, 'updatePassword'])->name('profile.updatePassword');
 
 Route::post('/update_fitness_goal', [UserController::class, 'updateFitnessGoal'])->name('update_fitness_goal');
-Route::get('/gymplanning', [UserController::class, 'showGymPlanner'])->name('gymplanning')->middleware(\App\Http\Middleware\PlanningMiddleWare::class);;
-Route::get('/mealplanning', [UserController::class, 'showMealPlanner'])->name('mealplanning')->middleware(\App\Http\Middleware\PlanningMiddleWare::class);;
+Route::get('/gymplanning', [UserController::class, 'showGymPlanner'])->name('gymplanning')->middleware(\App\Http\Middleware\PlanningMiddleWare::class);
+Route::get('/mealplanning', [UserController::class, 'showMealPlanner'])->name('mealplanning')->middleware(\App\Http\Middleware\PlanningMiddleWare::class);
 Route::post('/update_meal_preference', [UserController::class, 'updateMealPreference'])->name('update_meal_preference');
 Route::get('/onlinecoaching', [UserController::class, 'searchVideos'])->name('onlinecoaching');
 
